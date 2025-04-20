130e49041a7c3d68ec7f4ddb20aefb4dfd446150
Pancham Sardar <sardarpancham510510@gmail.com>
2025-04-20 14:10:37 +0530

    sidebar

--- exam-portal/src/app/page.tsx
+++ exam-portal/src/app/page.tsx
@@ -1,10 +1,9 @@
+import { MainSidebar } from "@/components/Mainbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="bg-blue-500 text-white px-4 py-2 rounded">
-        Start Exam
-      </div>
-    </div>
+    <>
+      <MainSidebar />
+    </>
   );
 }
